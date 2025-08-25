e15e519f2af664b25756bf7f2888acc3144b806e
VigneshKS-QA <vigneshkssomu26@gmail.com>
2025-08-25 14:43:40 +0530

    Cloned

--- src/main/java/demo/HelloWorld.java
+++ src/main/java/demo/HelloWorld.java
@@ -3,5 +3,7 @@ package demo;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Git + Java + Maven!");
+        System.out.println("Hello this is clone git");
+        System.out.println("Hello this is clone git");
     }
 }
